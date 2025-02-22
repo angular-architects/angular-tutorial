e49774c1f2a9f89eb16e35c922d3207f862a8621
Manfred Steyer <manfred.steyer@gmx.net>
2025-02-22 21:51:24 +0100

    solution

--- src/app/flight-edit/flight-edit.component.ts
+++ src/app/flight-edit/flight-edit.component.ts
@@ -1,17 +1,64 @@
-import { booleanAttribute, Component, input, numberAttribute, OnInit } from '@angular/core';
+import {
+  booleanAttribute,
+  Component,
+  inject,
+  input,
+  numberAttribute,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FlightService } from '../flight.service';
+import { Flight } from '../data/flight';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-edit',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './flight-edit.component.html',
-  styleUrl: './flight-edit.component.css'
+  styleUrl: './flight-edit.component.css',
 })
 export class FlightEditComponent implements OnInit {
+  private flightService = inject(FlightService);
 
   id = input.required({ transform: numberAttribute });
   showDetails = input.required({ transform: booleanAttribute });
 
+  from = signal('');
+  to = signal('');
+  date = signal('');
+
+  error = signal('');
+
   ngOnInit(): void {
-    console.log('id', this.id())
+    this.flightService.findById(this.id()).subscribe((flight) => {
+      this.setFlight(flight);
+    });
+  }
+
+  private setFlight(flight: Flight) {
+    this.from.set(flight.from);
+    this.to.set(flight.to);
+    this.date.set(flight.date);
+  }
+
+  save(): void {
+    this.error.set('');
+
+    const flight: Flight = {
+      id: this.id(),
+      from: this.from(),
+      to: this.to(),
+      date: this.date(),
+    };
+
+    this.flightService.save(flight).subscribe({
+      next: (flight) => {
+        this.setFlight(flight);
+      },
+      error: (err) => {
+        this.error.set(err.error ?? err.message ?? err);
+        throw err;
+      }
+    });
   }
 }
--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -3,6 +3,8 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from './data/flight';
 
+const url = 'https://demo.angulararchitects.io/api/flight';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,9 +18,25 @@ export class FlightService {
       accept: 'application/json'
     };
 
-    const url = 'https://demo.angulararchitects.io/api/flight';
-
     return this.http.get<Flight[]>(url, { params, headers });
+  }
+
+  findById(id: number): Observable<Flight> {
+    const params = { id };
+
+    const headers = {
+      accept: 'application/json'
+    };
+
+    return this.http.get<Flight>(url, { params, headers });
+  }
+
+  save(flight: Flight): Observable<Flight> {
+    const headers = {
+      accept: 'application/json'
+    };
+
+    return this.http.post<Flight>(url, flight, { headers });
   }
 
 }
